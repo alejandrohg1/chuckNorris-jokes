fb89aee41616106525702a66c4ca0d1bbcd5f7f6
alejandrohg1 <59666985+alejandrohg1@users.noreply.github.com>
2023-05-01 21:37:53 -0600

    Update main.jsx
    fixing wrong path from import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from "react"
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
